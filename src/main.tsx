9ef75ba656e7be16cf00217e94ee154a54f7eca3
batista0523 <elisaulbatista@pursuit.org>
2025-06-19 18:24:26 -0400

    added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { HelmetProvider } from "react-helmet-async";
